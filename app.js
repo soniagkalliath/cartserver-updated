016eff021c2843795506c7a05ddf138a1d0d5bd7
soniagkalliath <soniageorge111@gmail.com>
2023-04-02 17:20:04 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('./db/connection')
 const router = require('./routes/router')
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(router)
